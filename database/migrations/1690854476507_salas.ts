8a9998f2f7c54485c1ea7c44fe59e9565f1c94a8
Alexandre de Freitas Souza <alexandrefsoficial@gmail.com>
2023-08-02 18:58:58 -0300

    Fix: id adicionado como primary key em salas

--- database/migrations/1690854476507_salas.ts
+++ database/migrations/1690854476507_salas.ts
@@ -5,7 +5,8 @@ export default class extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.bigInteger('numero').primary().notNullable().unique()
+      table.increments('id')
+      table.bigInteger('numero').notNullable().unique()
       table.integer('capacidade').notNullable().unsigned()
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
